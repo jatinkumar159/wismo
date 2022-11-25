a331fd6204a959393fe863b07c234f739f8902f2
Raghav <raghav.kanwal@unicommerce.com>
2022-11-25 21:43:32 +0530

    Confirmation page

--- pages/confirmation.tsx
+++ pages/confirmation.tsx
@@ -5,7 +5,7 @@ import styles from './confirmation/confirmation.module.scss';
 
 export default function Confirmation() {
     return (
-        <div className={`${styles.container} confirmation`}>
+        <Box className={`${styles.container} confirmation`}>
             <Box className={styles.section}>
                 <div className={`${styles.sectionContent} mobile-section`}>
                     <p>Creating an order with <span className={styles.mobileNumber}>+91 9654723413</span>
@@ -37,10 +37,55 @@ export default function Confirmation() {
             </Box>
 
             <Box className={styles.section}>
-                <Box className={`${styles.sectionContent} delivery-section`} mb={4}>
-                    <Text mt={4}>Order Summary</Text>
+                <Box className={`${styles.sectionContent} order-summary`} mb={4}>
+                    <Text as="p" mt={2} mb={4}>Order Summary</Text>
+
+                    <Box justifyContent="space-between" flexDir="column">
+                        <Flex justifyContent='space-between' flexDir="row" mb={2}>
+                            <Text as="strong" className="key">Rs. 3,298</Text>
+                            <Text as="span" className={`value ${styles.viewToggle}`} justifyContent={'flex-end'}> <SmallAddIcon/> View More</Text>
+                        </Flex>
+                    </Box>
+
+                    <Flex mt={4} mb={4} flexDir="row">
+                        <img src="https://m.media-amazon.com/images/I/81cB0YABm3L._SL1500_.jpg" width="25%"></img>
+                        <Flex grow="1" flexDir={"column"} alignItems={"flex-start"} justifyContent={'center'} pl={4}>
+                            <Text fontSize="lg" color="gray-800">Baggit Yellow - XL</Text>
+                            <Text>Quantity: 1</Text>
+                            <Text>MRP: Rs. 3,298</Text>
+                        </Flex>
+                    </Flex>
+
+                    <Box justifyContent="space-between" flexDir="column">
+                        
+                        <Flex justifyContent='space-between' flexDir="row" mb={2}>
+                            <Text as="span" className="key">Subtotal</Text>
+                            <Text as="span" className="value" justifyContent={'flex-end'}>Rs. 3,298</Text>
+                        </Flex>
+                        <Flex justifyContent='space-between' flexDir="row" mb={2}>
+                            <Text as="span" className="key">Coupon Discount</Text>
+                            <Text as="span" className="value">Rs. 0</Text>
+                        </Flex>
+                        <Flex justifyContent='space-between' flexDir="row" mb={2}>
+                            <Text as="span" className="key">Shipping Fee</Text>
+                            <Text as="span" className="value">Rs. 0</Text>
+                        </Flex>
+                        <Flex justifyContent='space-between' flexDir="row" mb={2}>
+                            <Text as="span" className="key">Grand Total</Text>
+                            <Text as="strong" className="value">Rs. 3,298</Text>
+                        </Flex>
+                    </Box>
                 </Box>
             </Box>
-        </div>
+
+            <Box>
+            <Box className={styles.section}>
+                <Box className={`${styles.sectionContent} mobile-section`} mt={2} mb={2}>
+                    <Text>Pay via</Text>
+                </Box>
+            </Box>
+
+            </Box>
+        </Box>
     )
 }
