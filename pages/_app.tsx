3037c249233aace93dbb6218456e7ce87e5ec1e9
Raghav <raghav.kanwal@unicommerce.com>
2022-11-25 15:36:36 +0530

    Common Navigation area for Turbo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
+import Navigation from './components/Navigation/Navigation'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Navigation />
       <Component {...pageProps} />
     </ChakraProvider>
   )
---
+++ pages/profile.tsx
@@ -0,0 +1,7 @@
+import styles from "../styles/Profile.module.scss";
+
+export default function ProfilePage() {
+    return (
+        <div className={styles.container}>Profile Page</div>
+    )
+}
