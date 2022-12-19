9834ab79fbd7a799f5e8e22b6eef64d876590c03
Raghav <raghav.kanwal@unicommerce.com>
2022-12-19 22:09:44 +0530

    Centralised APIs using Gateway (ForAiur)

--- components/AddressCard/AddressCard.tsx
+++ components/AddressCard/AddressCard.tsx
@@ -24,32 +24,32 @@ export default function AddressCard({ address, selected, isInForm }: Props) {
     }
 
     return (
-        <Box p={4} mb={2} mt={2}>
+        <Box>
             <Flex align={'flex-start'} gap={'0.5rem'}>
                 <Flex grow={1} flexDir={'column'}>
                     {!isInForm ? (
-                        <Flex flexDir={`row`} gap={`4`}>
-                            <Text as="span" className={styles.selectedAddress}>
+                        <Flex flexDir={`row`} gap={`4`} align={`flex-start`}>
+                            <Text lineHeight={1} as="span" className={styles.selectedAddress}>
                                 <CheckCircleIcon color={'chakra-colors-green-400'} />
                             </Text>
-                            <Text className={styles.cardName} flexGrow={1}>
-                                <Text as="span" fontSize={'md'}>{address.name}</Text>
+                            <Text className={styles.cardName} flexGrow={1} lineHeight={1}>
+                                <Text as="span" fontSize={'md'} fontWeight={700}>{address.name}</Text>
                                 <Text as="span" className={styles.cardChip}>{address.address_type}</Text>
                             </Text>
                         </Flex>
-                    ) : <Text className={styles.cardName} flexGrow={1}>
-                        <Text as="span" fontSize={'md'}>{address.name}</Text>
-                        <Text as="span" className={styles.cardChip}>{address.address_type}</Text>
-                        <IconButton
-                            variant='ghost'
-                            colorScheme='teal'
-                            aria-label='Edit Address'
-                            onClick={handleAddressClick}
-                            icon={<EditIcon />}
-                        ></IconButton>
-                    </Text>}
+                    ) : <>
+                        <Flex flexDir={`row`} align={`center`}>
+                            <Text as="span" className={styles.cardName} flexGrow={1} lineHeight={1}>
+                                <Text as="span" fontSize={'md'}>{address.name}</Text>
+                                <Text as="span" className={styles.cardChip}>{address.address_type}</Text>
+                            </Text>
+                            <Text className={styles.editCardLink} lineHeight={1} as="span" onClick={handleAddressClick}>Edit</Text>
+                        </Flex>
+                    </>
+                    }
                     <Box mt={2}>
-                        <Text>{`${address.address_line1 + ', ' + (address.address_line2 ? address.address_line2 + ', ' : '') + address.city + ', ' + (address.district ? address.district + ', ' : '') + address.state + ' - ' + address.pincode}`}</Text>
+                        <Text mb={2} fontSize={`xs`}>{`${address.address_line1 + ', ' + (address.address_line2 ? address.address_line2 + ', ' : '') + address.city + ', ' + (address.district ? address.district + ', ' : '') + address.state + ' - ' + address.pincode}`}</Text>
+                        <Text fontSize={`xs`}>Mobile: {`${address.mobile}`}</Text>
                     </Box>
                 </Flex>
             </Flex>
