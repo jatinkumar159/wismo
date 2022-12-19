4b5dacc00ce068c4b89416ef8ea1771dd2b7b839
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2022-12-20 00:42:15 +0530

    Add headers for content-type

--- apis/gateway.ts
+++ apis/gateway.ts
@@ -1,12 +1,9 @@
 import { getHeaders } from './../utils/headers';
 
 export default async function gateway(path: string, method: string = 'GET', payload?: any) {
-    const headers = getHeaders();
-    if(method === 'POST') headers.append('Content-Type', 'application/json');
-
     return fetch(path, {
         method,
-        headers: headers,
+        headers: getHeaders(),
         body: method === 'GET' ? null : payload
     });
 }
