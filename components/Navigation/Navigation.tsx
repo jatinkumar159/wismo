2be91b1a553cd8b03ea3766b33b4f2ef7847df7e
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2022-12-22 10:02:21 +0530

    Add Titles

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -3,13 +3,16 @@ import { IconButton, Text } from '@chakra-ui/react';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { selectFlowMap } from '../../redux/slices/navigationSlice';
 import { selectIsVerified, selectPhone, unsetPhone } from '../../redux/slices/profileSlice';
 import styles from './Navigation.module.scss';
 
 export default function Navigation() {
     const router = useRouter();
     const dispatch = useAppDispatch();
+
     const phone = useAppSelector(selectPhone);
+    const flowMap = useAppSelector(selectFlowMap);
     const isVerified = useAppSelector(selectIsVerified);
 
     const handleBackNavigation = () => {
@@ -28,8 +31,8 @@ export default function Navigation() {
     return (
         <div className={styles.container}>
             <div className={styles.brand}>
-                {!(router.pathname === '/profile' && !phone) ? <IconButton aria-label="back" icon={<ArrowBackIcon />} background={"transparent"} _hover={{ bg: 'transparent' }} onClick={handleBackNavigation} /> : <IconButton aria-label="close" icon={<SmallCloseIcon />} background={"transparent"} _hover={{ bg: 'transparent' }} onClick={handleClose} />} 
-                <Text as="span" fontSize="sm" fontWeight="bold">TITLE</Text>
+                {!(router.pathname === '/profile' && !phone) ? <IconButton aria-label="back" icon={<ArrowBackIcon />} background={"transparent"} _hover={{ bg: 'transparent' }} onClick={handleBackNavigation} /> : <IconButton aria-label="close" icon={<SmallCloseIcon />} background={"transparent"} _hover={{ bg: 'transparent' }} onClick={handleClose} />}
+                <Text as="span" fontSize="sm" fontWeight="bold">{flowMap[router.pathname].title}</Text>
             </div>
             <div className={styles.attribution}>
                 <Image src={'https://infowordpress.s3.ap-south-1.amazonaws.com/wp-content/uploads/2022/10/06114711/logo.webp'} alt="Logo" width='70' height='50' priority />
--- redux/slices/navigationSlice.ts
+++ redux/slices/navigationSlice.ts
@@ -2,13 +2,38 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { AppState } from "../store";
 
 // firstLoad for a key is undefined before first load, true after first load and before next load, and false after second load.
-export interface NavigationState {
+interface NavigationState {
     firstLoad: {[page: string] : boolean};
+    flowMap: {[page: string] : FlowState};
+}
+
+interface FlowState {
+    title: string;
 }
 
 const initialState: NavigationState = {
     firstLoad: {
         'addresses': true,
+    },
+    flowMap: {
+        '/profile': {
+            title: 'MOBILE',
+        },
+        '/addresses': {
+            title: 'ADDRESS',
+        },
+        '/add-address': {
+            title: 'ADDRESS',
+        },
+        '/edit-address': {
+            title: 'ADDRESS',
+        },
+        '/discounts': {
+            title: 'PROMOS',
+        },
+        '/confirmation': {
+            title: 'PAYMENT',
+        },
     }
 }
 
@@ -25,5 +50,6 @@ export const navigationSlice = createSlice({
 export const { setFirstLoad } = navigationSlice.actions;
 
 export const selectFirstLoad = (state: AppState) => state.navigation.firstLoad;
+export const selectFlowMap = (state: AppState) => state.navigation.flowMap;
 
 export default navigationSlice.reducer;
