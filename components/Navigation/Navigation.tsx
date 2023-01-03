c2cbac7b90c1ba151431e65fb106a64610b97f59
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2023-01-03 13:25:44 +0530

    Redesign Navigation bar

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,12 +1,19 @@
 import Image from 'next/image';
 import imageLoader from '../../utils/imageLoader';
 import styles from './Navigation.module.scss';
+import { Flex } from '@chakra-ui/react';
 
 export default function Navigation() {
     return (
         <div className={styles.container}>
+            <div className={styles.brand}>
+                <Image loader={imageLoader} src='https://cdn.worldvectorlogo.com/logos/reddit-logo-new.svg' alt='Brand' width='60' height='40'></Image>
+            </div>
             <div className={styles.attribution}>
-                <Image loader={imageLoader} src={'https://infowordpress.s3.ap-south-1.amazonaws.com/wp-content/uploads/2022/10/06114711/logo.webp'} alt="Logo" width='70' height='50' priority />
+                <Flex dir='row' alignItems='center'>
+                    <span>Powered by </span>
+                    <Image loader={imageLoader} src='https://infowordpress.s3.ap-south-1.amazonaws.com/wp-content/uploads/2022/10/06114711/logo.webp' alt="Unicommerce" width='60' height='40' priority />
+                </Flex>
             </div>
         </div>
     )
