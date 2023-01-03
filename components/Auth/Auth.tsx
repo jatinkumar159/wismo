1400a7efb94b5af29a1b7d0737adb7d1a88eb825
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2023-01-03 21:27:48 +0530

    Add Login bottom drawer

--- components/Auth/Auth.tsx
+++ components/Auth/Auth.tsx
@@ -1,14 +1,44 @@
-import { Box, Text } from "@chakra-ui/react"
+import { CloseIcon } from "@chakra-ui/icons";
+import { Box, Button, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, Flex, HStack, PinInput, PinInputField, Text, useDisclosure } from "@chakra-ui/react"
+import { useState } from "react";
 import styles from "./Auth.module.scss"
 
 export default function Auth() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [pin, setPin] = useState<string>('');
+
     return (
-        <Box className={styles.container}>
-            <Text>
-                You don&apos;t have access to take any action on
-                this page. <Text color='blue' as='span'>Login</Text> to see
-                order details or share feedback
-            </Text>
-        </Box>
+        <>
+            <Box className={styles.container}>
+                <Text>
+                    You don&apos;t have access to take any action on
+                    this page. <Text color='blue' as='span' cursor='pointer' onClick={onOpen}>Login</Text> to see
+                    order details or share feedback
+                </Text>
+            </Box>
+            <Drawer placement="bottom" onClose={onClose} isOpen={isOpen}>
+                <DrawerOverlay />
+                <DrawerContent borderRadius='1rem 1rem 0 0'>
+                    <DrawerHeader marginLeft='auto'>
+                        <CloseIcon w='1rem' h='1rem' onClick={onClose} />
+                    </DrawerHeader>
+                    <DrawerBody>
+                        <Flex flexDir='column' gap='1rem' pb='3rem' paddingInline='1rem'>
+                            <Text>Enter OTP received on 817*****70</Text>
+                            <HStack>
+                                <PinInput otp onChange={val => setPin(val)}>
+                                    <PinInputField />
+                                    <PinInputField />
+                                    <PinInputField />
+                                    <PinInputField />
+                                </PinInput>
+                            </HStack>
+                            <Text>Resend OTP in 25 seconds</Text>
+                            <Button fontSize='sm' bg='black' color='white' _hover={{ background: 'black' }} textTransform='uppercase' onClick={onClose}>Login</Button>
+                        </Flex>
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
+        </>
     )
 }
