162ea37b48e5d7ee4522554e132138c0805c59ea
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2023-01-03 19:32:03 +0530

    Create layout for order details

--- components/Auth/Auth.tsx
+++ components/Auth/Auth.tsx
@@ -4,7 +4,11 @@ import styles from "./Auth.module.scss"
 export default function Auth() {
     return (
         <Box className={styles.container}>
-            <Text>Auth!</Text>
+            <Text>
+                You don&apos;t have access to take any action on
+                this page. <Text color='blue' as='span'>Login</Text> to see
+                order details or share feedback
+            </Text>
         </Box>
     )
 }
--- components/Details/Details.tsx
+++ components/Details/Details.tsx
@@ -1,10 +1,47 @@
-import { Box, Text } from "@chakra-ui/react"
+import { ChevronRightIcon, CopyIcon } from "@chakra-ui/icons"
+import { Avatar, AvatarGroup, Box, Divider, Flex, Icon, Text } from "@chakra-ui/react"
 import styles from "./Details.module.scss"
+import { MdLocationPin } from 'react-icons/md'
+import { FaTruckMoving } from 'react-icons/fa'
+import { BsHeadphones } from 'react-icons/bs'
 
 export default function Details() {
     return (
-        <Box className={styles.container}>
-            <Text>Details!</Text>
-        </Box>
+        <Flex className={styles.container} flexDir='column' gap='0.5rem'>
+            <Text>Order Details</Text>
+            <Flex justifyContent='space-between' alignItems='center'>
+                <AvatarGroup size='sm' max={2}>
+                    <Avatar></Avatar>
+                    <Avatar></Avatar>
+                    <Avatar></Avatar>
+                    <Avatar></Avatar>
+                </AvatarGroup>
+                <ChevronRightIcon w='1.5rem' h='1.5rem' />
+            </Flex>
+            <Text>Order No FK989473</Text>
+            <Box>
+                <Icon as={MdLocationPin} w='1.5rem' h='1.5rem' />
+                <Text as='span'>Gurgaon</Text>
+            </Box>
+            <Divider />
+            <Flex>
+                <Icon as={FaTruckMoving} mr='0.5rem'></Icon>
+                <Box>
+                    <Text>Delhivery</Text>
+                    <Text>Tracking ID: 123456789345 <CopyIcon /></Text>
+                </Box>
+            </Flex>
+            <Divider />
+            <Flex justifyContent='space-between'>
+                <Flex>
+                    <Icon as={BsHeadphones} mr='0.5rem'></Icon>
+                    <Box>
+                        <Text>Need Help</Text>
+                        <Text>Raise Ticket</Text>
+                    </Box>
+                </Flex>
+                <ChevronRightIcon w='1.5rem' h='1.5rem' />
+            </Flex>
+        </Flex>
     )
 }
