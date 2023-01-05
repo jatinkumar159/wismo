7565dc9713f4e45ca5c30c2f703251fe7c4a94b4
Raghav <raghav.kanwal@unicommerce.com>
2023-01-05 15:27:58 +0530

    Item List

--- components/ItemList/ItemList.tsx
+++ components/ItemList/ItemList.tsx
@@ -1,4 +1,4 @@
-import { Divider, Flex, HStack, Icon, Text } from '@chakra-ui/react';
+import { Box, Divider, Flex, HStack, Icon, Text } from '@chakra-ui/react';
 import { MdLocationPin } from 'react-icons/md';
 import styles from './ItemList.module.scss';
 
@@ -20,28 +20,30 @@ export default function ItemList() {
     return (
         <>
             <Divider />
-            {
-            products.map((item,itemIdx) => {
-            return <Flex key={itemIdx} flexGrow={1} my={2}>
-                <img src={item.image || 'https://via.placeholder.com/100'} width="25%" className={styles.image} />
-                <Flex grow="1" flexDir={"column"} alignItems={"flex-start"} justifyContent={'center'} pl={4}>
-                    {/* <Text fontSize={`xs`} color="var(--turbo-colors-light-text)" fontWeight={700}>{item.vendor}</Text> */}
-                    <Text fontSize={`sm`} color="var(--turbo-colors-light-text)">{item.product_title}</Text>
-                    {/* {
-                        item.options_with_values.map((option: any, optionIdx: number) => {
-                            return <Text mt={1} key={`option-${optionIdx}`} fontSize={`xs`}>{option.name}:{option.value}</Text>
-                        })
-                    } */}
-                </Flex>
-                <Flex flexDir={"column"} alignItems={"flex-end"} justifyContent={'center'} pl={4}>
-                    <Text fontSize={`xs`} color="var(--turbo-colors-light-text)">
-                        <Text as="span" className={styles.lightText}>Quantity:&nbsp;{item.quantity}</Text>
-                    </Text>
-                    <Text fontSize={`xs`} color="var(--turbo-colors-light-text)">₹ {(item.price / 100).toFixed(2)}</Text>
-                </Flex>
-            </Flex>
-            })
-            }
+            <Box className={styles.itemCarousel}>
+                {
+                    products.map((item,itemIdx) => {
+                    return <Flex key={itemIdx} flexGrow={1} my={2}>
+                        <img src={item.image || 'https://via.placeholder.com/100'} width="25%" className={styles.image} />
+                        <Flex grow="1" flexDir={"column"} alignItems={"flex-start"} justifyContent={'center'} pl={4}>
+                            {/* <Text fontSize={`xs`} color="var(--turbo-colors-light-text)" fontWeight={700}>{item.vendor}</Text> */}
+                            <Text fontSize={`sm`} color="var(--turbo-colors-light-text)">{item.product_title}</Text>
+                            {/* {
+                                item.options_with_values.map((option: any, optionIdx: number) => {
+                                    return <Text mt={1} key={`option-${optionIdx}`} fontSize={`xs`}>{option.name}:{option.value}</Text>
+                                })
+                            } */}
+                        </Flex>
+                        <Flex flexDir={"column"} alignItems={"flex-end"} justifyContent={'center'} pl={4}>
+                            <Text fontSize={`xs`} color="var(--turbo-colors-light-text)">
+                                <Text as="span" className={styles.lightText}>Quantity:&nbsp;{item.quantity}</Text>
+                            </Text>
+                            <Text fontSize={`xs`} color="var(--turbo-colors-light-text)">₹ {(item.price / 100).toFixed(2)}</Text>
+                        </Flex>
+                    </Flex>
+                    })
+                }
+            </Box>
             <Divider my={2}/>
             <Flex flexDir="row" justify="space-between" my={4} >
                 <Text fontSize={`sm`} color="var(--turbo-colors-light-text)">Grand Total: </Text>
