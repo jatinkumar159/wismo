96bb3342225f044c152aab2350e042f4e42610cd
Raghav <raghav.kanwal@unicommerce.com>
2023-01-05 23:20:56 +0530

    Added Query Params to request

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,62 @@
 import { ChevronRightIcon } from "@chakra-ui/icons";
-import { Button, Flex, Heading, Text, Input, space, useQuery } from "@chakra-ui/react";
+import { Button, Flex, Heading, Text, Input, space, useQuery, FormControl, FormErrorMessage } from "@chakra-ui/react";
+import { ErrorMessage, Form, Formik, useFormik } from "formik";
 import { useRouter } from "next/router";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { fetchTracking } from "../apis/post";
 import styles from './index.module.scss';
+import * as Yup from 'yup'
 
 export default function Home() {
   const router = useRouter();
   const [orderID, setOrderID] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
   
-  const tracking = fetchTracking('53441108954');
+  const trackingNumberRef = useRef<HTMLInputElement>(null);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setOrderID(e.target.value);
+  useEffect(() => {
+    trackingNumberRef?.current?.focus();
+  }, []);
+
+  const validationSchema = Yup.object().shape({
+    trackingNumber: Yup.string().min(3, 'Tracking number should be at least 3 digits').required('Tracking number is required')
+  })
+
+  const tryFetchTracking = async (trackingNumber: string): Promise<Boolean> => {
+    const trackDetails = await fetchTracking(trackingNumber);
+    return router.push({
+      pathname: '/order',
+      query: {
+        id: trackingNumber
+      }
+    });
   }
 
-  const handleClick = () => {
-    console.log(orderID);
-    router.push('/order');
-  }
-
-  return <>
-  <Flex className={styles.pageContainer} flexDir="column" h={`100%`} align="center" py={4} gap={`0.5rem`} mt={10}>
-    <Text as="h1" marginInline='auto' fontSize='xl'>Track your order</Text>
-    <Input className={styles.trackInput} fontSize="md" placeholder="Enter Order ID or Tracking Number" border='1px' borderColor='black' borderStyle='solid' value={orderID} onChange={handleChange} textAlign="center" w={`auto`}/>
-    <Button mt={4}fontSize='sm' bg='black' color='white' _hover={{ background: 'black' }} w='auto' px={8} onClick={handleClick}>Track&nbsp;<ChevronRightIcon /></Button>
-  </Flex>
-  </>
+  return (<>
+    <Flex className={styles.pageContainer} flexDir="column" h={`100%`} align="center" py={4} gap={`0.5rem`} mt={10}>
+      <Text as="h1" marginInline='auto' fontSize='xl'>Track your order</Text>
+      <Formik w={`100%`}
+        initialValues={{
+          trackingNumber: ""
+        }}
+        validationSchema = {validationSchema} 
+        validateOnBlur={false}
+        onSubmit={(values: any) => {
+          return tryFetchTracking(values.trackingNumber);
+        }}>
+          {
+            ({errors, touched, values, handleChange, submitForm}) => (
+              <Form>
+                <span>{touched.trackingNumber}</span>
+                <FormControl isInvalid={touched.trackingNumber && errors.trackingNumber?.length ? true : false}>
+                  <Input w={`260px`} textAlign="center" placeholder="Tracking Number or Order ID" autoFocus name="trackingNumber" type="text" onChange={handleChange} value={values.trackingNumber} ref={trackingNumberRef}/>
+                  <FormErrorMessage fontSize="xs">{errors.trackingNumber}</FormErrorMessage>
+                </FormControl>
+                <Button w={`100%`} mt={4}fontSize='sm' bg='black' color='white' _hover={{ background: 'black' }} px={8} onClick={submitForm}>Track&nbsp;<ChevronRightIcon /></Button>
+              </Form>
+            )
+          }
+          </Formik>
+    </Flex>
+  </>)
 }
