9f6076a579b07f7c44e7ba7835e0874e9311db84
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2023-01-16 15:36:34 +0530

    Remove redundant headers

--- utils/headers.ts
+++ utils/headers.ts
@@ -1,13 +1,12 @@
 export function getHeaders(method: string, headerSource: string | undefined): Headers {
   const headers = new Headers()
 
-  if (method === 'POST') headers.append('Content-type', 'application/json')
-
   if (headerSource === 'otp') {
     headers.append('Authorization', 'Basic dXNlcjpwYXNzd29yZA==')
   } else {
     headers.append('APP-KEY', '#$%^SK&SNLSH*^%SF')
   }
-  if(method !== 'GET') headers.append('Content-type', 'application/json');
+  if (method !== 'GET') headers.append('Content-Type', 'application/json')
+
   return headers
 }
